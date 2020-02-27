18798f054e76eaaf9ec800d983ebc54b8189b9ae
fed85 <61115833+fed85@users.noreply.github.com>
2020-02-27 21:11:14 +0700

    shot.php

--- shot.php
+++ shot.php
@@ -56,18 +56,18 @@ echo color("green"," =================================== \n");
 				save("token.txt",$token);
 				
 				echo color("green","\n===========(REDEEM VOUCHER)===========");
-				echo "\n".color("yellow","!] Claim Voc GOFOOD022620A");
+				echo "\n".color("yellow","!] Claim Voc GOFOOD021120A");
 				echo "\n".color("yellow","!] Please wait...");
 				for($a=1;$a<=3;$a++){
 					echo color("yellow",".");
 					sleep(1);
 				}
-				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD021120A"}');
 				$message = fetch_value($code1,'"message":"','"');
 				if(strpos($code1, 'You can use this promo now...')){
 					echo "\n".color("green","+] Message: ".$message);
 					goto goride;
-				}else{
+				}else{a
 					echo "\n".color("red","-] Message: ".$message);
 					
 					echo "\n".color("yellow","!] Claim Voc GOFOOD022620A");
@@ -97,15 +97,15 @@ echo color("green"," =================================== \n");
 					$message1 = fetch_value($goride,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message1);
 							
-					echo "\n".color("yellow","!] Claim Voc COBAGORIDE");
+					echo "\n".color("yellow","!] Claim Voc GOFOOD022620A");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
-					$message2 = fetch_value($goride1,'"message":"','"');
+					$gofood = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+					$message2 = fetch_value($gofood,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
 					sleep(3);
 					
